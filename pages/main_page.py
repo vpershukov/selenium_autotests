9287a293dc3bbd0eb6c164a317411512141ad0a8
Vladimir  Pershukov <v.pershukov@appboost42.com>
2021-06-05 01:56:05 +0300

    [feat] Реализация LoginPage

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,8 +1,8 @@
 from .base_page import BasePage
-from selenium.webdriver.common.by import By
-
-
 from .locators import MainPageLocators
+
+
+from selenium.webdriver.common.by import By
 
 
 class MainPage(BasePage):
--- test_main_page.py
+++ test_main_page.py
@@ -1,17 +1,37 @@
 from .pages.main_page import MainPage
-
-link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    # link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    url = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
+    page = MainPage(browser, url)
     page.open()
     page.go_to_login_page()
 
 
 def test_guest_should_see_login_link(browser):
-    # link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    url = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
+    page = MainPage(browser, url)
     page.open()
     page.should_be_login_link()
+
+
+def test_guest_should_see_current_login_page(browser):
+    url = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, url)
+    page.open()
+    page.should_be_login_url()
+
+
+def test_guest_should_see_login_form(browser):
+    url = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, url)
+    page.open()
+    page.should_be_login_form()
+
+
+def test_guest_should_see_register_form(browser):
+    url = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, url)
+    page.open()
+    page.should_be_register_form()
