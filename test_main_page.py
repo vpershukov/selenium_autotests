75a893a2da8205bed07fbcb2cd38ebe82560850e
Vladimir  Pershukov <v.pershukov@appboost42.com>
2021-06-05 00:27:24 +0300

    [feat] Элементы страниц в паттерне Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,15 +1,17 @@
 from .pages.main_page import MainPage
 
+link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
+
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    # link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
 
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    # link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
