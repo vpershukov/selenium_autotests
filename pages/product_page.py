aa108a8d80d472e9aa65d4c5a290a2093aff90bd
Vladimir  Pershukov <v.pershukov@appboost42.com>
2021-06-06 13:33:53 +0300

    [feat] Независимость от данных

--- pages/product_page.py
+++ pages/product_page.py
@@ -7,20 +7,31 @@ class PageObject(BasePage):
         add_to_card_btn = self.browser.find_element(*ProductPageLocators.ADD_TO_CARD_BTN)
         add_to_card_btn.click()
 
-    def should_be_item_name_in_card_message(self):
-        item_name = self.browser.find_element(*ProductPageLocators.ITEM_NAME).text
-        added_to_card_item_name = self.browser.find_element(
+    def get_item_name(self):
+        return self.browser.find_element(*ProductPageLocators.ITEM_NAME).text
+
+    def get_added_to_card_item_name(self):
+        return self.browser.find_element(
             *ProductPageLocators.ADDED_TO_CARD_ITEM_NAME).text
 
+    def get_item_price(self):
+        return self.browser.find_element(
+            *ProductPageLocators.ITEM_PRICE).text
+
+    def get_item_price_in_card_info(self):
+        return self.browser.find_element(
+            *ProductPageLocators.ADDED_TO_CARD_ITEM_PRICE).text
+
+    def should_be_item_name_in_card_message(self):
+        item_name = self.get_item_name()
+        added_to_card_item_name = self.get_added_to_card_item_name()
         error_info = "Item name not equal to added to card item name"
 
         assert item_name == added_to_card_item_name, error_info
 
     def should_be_item_price_in_card_info(self):
-        item_price = self.browser.find_element(*ProductPageLocators.ITEM_PRICE).text
-        added_to_card_item_price = self.browser.find_element(
-            *ProductPageLocators.ADDED_TO_CARD_ITEM_PRICE).text
-
+        item_price = self.get_item_price()
+        added_to_card_item_price = self.get_item_price_in_card_info()
         error_info = "Item price not equal to added to card item price"
 
         assert item_price == added_to_card_item_price, error_info
--- test_product_page.py
+++ test_product_page.py
@@ -2,10 +2,11 @@ from .pages.product_page import PageObject
 
 
 NEW_YEAR_PROMO_URL = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+NEW_YEAR_2019_PROMO_URL = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
 
 
 def test_guest_able_to_add_item_to_card(browser):
-    page = PageObject(browser, NEW_YEAR_PROMO_URL)
+    page = PageObject(browser, NEW_YEAR_2019_PROMO_URL)
     page.open()
     page.add_item_to_card()
     page.solve_quiz_and_get_code()
