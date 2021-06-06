157d17974e54123871a0e89f6437a8b5a01e04ac
Vladimir  Pershukov <v.pershukov@appboost42.com>
2021-06-06 13:53:09 +0300

    [feat] Независимость контента, ищем баг

--- test_product_page.py
+++ test_product_page.py
@@ -1,12 +1,24 @@
 from .pages.product_page import PageObject
 
 
+import pytest
+
+
 NEW_YEAR_PROMO_URL = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
 NEW_YEAR_2019_PROMO_URL = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
+NO_PROMO_URL = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
 
-
-def test_guest_able_to_add_item_to_card(browser):
-    page = PageObject(browser, NEW_YEAR_2019_PROMO_URL)
+@pytest.mark.parametrize(
+    "promo_code",
+    [
+        "offer0", "offer1", "offer2", "offer3", "offer4",
+        "offer5", "offer6", pytest.param("offer7", marks=pytest.mark.skip),
+        "offer8", "offer9"
+    ]
+)
+def test_guest_able_to_add_item_to_card(browser, promo_code):
+    URL = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo={promo_code}"
+    page = PageObject(browser, URL)
     page.open()
     page.add_item_to_card()
     page.solve_quiz_and_get_code()
