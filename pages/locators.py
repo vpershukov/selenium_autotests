60e595b40acff4f8f4765ac3adc0466636ad6ef5
Vladimir  Pershukov <v.pershukov@appboost42.com>
2021-06-08 12:58:13 +0300

    [fix] Обновлены Locators

--- pages/locators.py
+++ pages/locators.py
@@ -2,19 +2,19 @@ from selenium.webdriver.common.by import By
 
 
 class BasePageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inv")
+    LOGIN_LINK = (By.ID, "login_link")
+    LOGIN_LINK_INVALID = (By.ID, "login_link_inv")
     BASKET_BTN = (By.CSS_SELECTOR, ".btn-group a.btn.btn-default")
     USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 
 class MainPageLocators():
-    REGISTER_LINK = (By.CSS_SELECTOR, "#registration_link")
+    REGISTER_LINK = (By.ID, "registration_link")
 
 
 class LoginPageLocators():
-    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
-    REGSTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    LOGIN_FORM = (By.ID, "login_form")
+    REGSTER_FORM = (By.ID, "register_form")
 
 
 class ProductPageLocators():
@@ -31,7 +31,7 @@ class ProductPageLocators():
 
 class BasketPageLocators():
     EMPTY_BASKET_INFO = (By.CSS_SELECTOR, ".content p")
-    BASKET_WO_ITEMS = (By.ID, "#basket_formset")
+    BASKET_WO_ITEMS = (By.ID, "basket_formset")
     REGISTER_USER_EMAIL = (By.ID, "id_registration-email")
     REGISTER_USER_PASS = (By.ID, "id_registration-password1")
     REGISTER_USER_PASS_CONFIRM = (By.ID, "id_registration-password2")
