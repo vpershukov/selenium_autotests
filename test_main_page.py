e559940174fe0b444bddc54330dfbc8c59aa4ef2
Vladimir  Pershukov <v.pershukov@appboost42.com>
2021-08-08 00:44:40 +0300

    [fix] URLS imports

--- test_main_page.py
+++ test_main_page.py
@@ -4,13 +4,13 @@ import pytest
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
-import project_urls
+from .project_urls import MAIN_PAGE, LOGIN_URL
 
 
 @pytest.mark.login_guest
 class TestLoginFromMainPage():
     def test_guest_can_go_to_login_page(self, browser):
-        page = MainPage(browser, project_urls.MAIN_PAGE)
+        page = MainPage(browser, MAIN_PAGE)
         page.open()
         page.go_to_login_page()
         login_page = LoginPage(browser, browser.current_url)
@@ -18,19 +18,19 @@ class TestLoginFromMainPage():
 
 
     def test_guest_should_see_login_link(self, browser):
-        page = MainPage(browser, project_urls.MAIN_PAGE)
+        page = MainPage(browser, MAIN_PAGE)
         page.open()
         page.should_be_login_link()
 
 
 def test_guest_should_see_current_login_page(browser):
-    page = LoginPage(browser, project_urls.LOGIN_URL)
+    page = LoginPage(browser, LOGIN_URL)
     page.open()
     page.should_be_login_page()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    page = MainPage(browser, project_urls.MAIN_PAGE)
+    page = MainPage(browser, MAIN_PAGE)
     page.open()
     page.should_be_basket_btn()
     page.go_to_basket()
--- test_product_page.py
+++ test_product_page.py
@@ -4,25 +4,25 @@ import pytest
 from .pages.product_page import PageObject
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
-import project_urls
+from .project_urls import LOGIN_URL, CODERS_AT_WORK, CITY_AND_STARS
 
 
 class TestUserAddToBasketFromProductPage():
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
-        self.login_page = LoginPage(browser, project_urls.LOGIN_URL)
+        self.login_page = LoginPage(browser, LOGIN_URL)
         self.login_page.open()
         self.login_page.register_new_user()
         self.login_page.should_be_authorized_user()
 
     def test_user_cant_see_success_message(self, browser):
-        page = PageObject(browser, project_urls.CODERS_AT_WORK)
+        page = PageObject(browser, CODERS_AT_WORK)
         page.open()
         page.should_not_be_success_message()
 
     @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
-        URL = f"{project_urls.CODERS_AT_WORK}?promo=offer0"
+        URL = f"{CODERS_AT_WORK}?promo=offer0"
         page = PageObject(browser, URL)
         page.open()
         page.add_item_to_card()
@@ -41,7 +41,7 @@ class TestUserAddToBasketFromProductPage():
     ]
 )
 def test_guest_can_add_product_to_basket(browser, promo_code):
-    URL = f"{project_urls.CODERS_AT_WORK}?promo={promo_code}"
+    URL = f"{CODERS_AT_WORK}?promo={promo_code}"
     page = PageObject(browser, URL)
     page.open()
     page.add_item_to_card()
@@ -51,14 +51,14 @@ def test_guest_can_add_product_to_basket(browser, promo_code):
 
 
 def test_guest_cant_see_success_message(browser):
-    page = PageObject(browser, project_urls.CODERS_AT_WORK)
+    page = PageObject(browser, CODERS_AT_WORK)
     page.open()
     page.should_not_be_success_message()
 
 
 @pytest.mark.skip(reason="Failed test for lesson 4.3")
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    page = PageObject(browser, project_urls.CODERS_AT_WORK)
+    page = PageObject(browser, CODERS_AT_WORK)
     page.open()
     page.add_item_to_card()
     page.should_not_be_success_message()
@@ -66,21 +66,21 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
 
 @pytest.mark.skip(reason="Failed test for lesson 4.3")
 def test_message_disappeared_after_adding_product_to_basket(browser):
-    page = PageObject(browser, project_urls.CODERS_AT_WORK)
+    page = PageObject(browser, CODERS_AT_WORK)
     page.open()
     page.add_item_to_card()
     page.should_be_disappeared_success_message()
 
 
 def test_guest_should_see_login_link_on_product_page(browser):
-    page = PageObject(browser, project_urls.CITY_AND_STARS)
+    page = PageObject(browser, CITY_AND_STARS)
     page.open()
     page.should_be_login_link()
 
 
 @pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
-    page = PageObject(browser, project_urls.CITY_AND_STARS)
+    page = PageObject(browser, CITY_AND_STARS)
     page.open()
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
@@ -89,7 +89,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
 
 @pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    page = PageObject(browser, project_urls.CODERS_AT_WORK)
+    page = PageObject(browser, CODERS_AT_WORK)
     page.open()
     page.should_be_basket_btn()
     page.go_to_basket()
